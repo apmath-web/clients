5fa7299b5469bb5052ca3aed2d9ff3083f9b4a6e
korwin <levozavr@mail.ru>
2019-03-12 14:35:01 +0300

    implement Jobs app model

--- Infrastructure/applicationModels/JobsApplicationModel.go
+++ Infrastructure/applicationModels/JobsApplicationModel.go
@@ -1,1 +1,21 @@
 package applicationModels
+
+type JobsApplicationModel struct {
+	name string
+	wage int
+}
+
+func (j *JobsApplicationModel) GetName() string {
+	return j.name
+}
+
+func (j *JobsApplicationModel) GetWage() int {
+	return j.wage
+}
+
+func (j *JobsApplicationModel) SetName(name string) {
+	j.name = name
+}
+func (j *JobsApplicationModel) SetWage(wage int) {
+	j.wage = wage
+}
