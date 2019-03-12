82e604b97560b6a9368c20cb63eff944048fd2ca
korwin <levozavr@mail.ru>
2019-03-12 14:37:30 +0300

    implement passport app model

--- Infrastructure/applicationModels/PassportApplicationModel.go
+++ Infrastructure/applicationModels/PassportApplicationModel.go
@@ -1,1 +1,21 @@
 package applicationModels
+
+type PassportApplicationModel struct {
+	series int
+	number int
+}
+
+func (p *PassportApplicationModel) GetSeries() int {
+	return p.series
+}
+
+func (p *PassportApplicationModel) GetNumber() int {
+	return p.number
+}
+
+func (p *PassportApplicationModel) SetNumber(number int) {
+	p.number = number
+}
+func (p *PassportApplicationModel) SetSeries(series int) {
+	p.series = series
+}
