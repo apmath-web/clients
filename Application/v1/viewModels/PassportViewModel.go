c8ebead6f8c04db74181efc1707ff419a5db5467
korwin <levozavr@mail.ru>
2019-03-12 14:11:28 +0300

    add json marshal for pasport

--- Application/v1/viewModels/PassportViewModel.go
+++ Application/v1/viewModels/PassportViewModel.go
@@ -1,5 +1,7 @@
 package viewModels
 
+import "encoding/json"
+
 type PassportViewModel struct {
 	series int
 	number int
@@ -11,4 +13,11 @@ func (p *PassportViewModel) GetSeries() int {
 
 func (p *PassportViewModel) GetNumber() int {
 	return p.number
+}
+
+func (p *PassportViewModel) MarshalJSON() (b []byte, e error) {
+	return json.Marshal(map[string]int{
+		"series": p.series,
+		"number": p.number,
+	})
 }
