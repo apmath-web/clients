d653f9988b0e293e7bcd1dc9a1f9411994ffa220
korwin <levozavr@mail.ru>
2019-03-12 12:11:49 +0300

    add jobs view model

--- Application/v1/viewModels/JobsViewModel.go
+++ Application/v1/viewModels/JobsViewModel.go
@@ -1,1 +1,14 @@
 package viewModels
+
+type JobsViewModel struct {
+	name string
+	wage int
+}
+
+func (j *JobsViewModel) GetName() string {
+	return j.name
+}
+
+func (j *JobsViewModel) GetWage() int {
+	return j.wage
+}
