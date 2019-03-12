fff61078fe5a656275dcf24cf8a83191842344f2
korwin <levozavr@mail.ru>
2019-03-12 11:13:14 +0300

    add viewmodel interfaces

--- Domain/viewModelInterface.go
+++ Domain/viewModelInterface.go
@@ -1,4 +1,22 @@
 package Domain
 
-type HelloWorldViewModel interface {
+type UserViewModel interface {
+	GetFirstName() string
+	GetLastName() string
+	GetBirthDate() string
+	GetPassport() PassportViewModel
+	GetJobs() []JobsViewModel
+	GetSex() string
+	GetMaritalStatus() string
+	GetChildren() int
+}
+
+type PassportViewModel interface {
+	GetSeries() int
+	GetNumber() int
+}
+
+type JobsViewModel interface {
+	GetName() string
+	GetWage() int
 }
