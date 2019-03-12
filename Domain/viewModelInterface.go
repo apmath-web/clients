0cb5f39ec7ce423962453c2a97384f9d59832020
korwin <levozavr@mail.ru>
2019-03-12 14:52:31 +0300

    refactor of interfaces

--- Domain/viewModelInterface.go
+++ Domain/viewModelInterface.go
@@ -1,24 +1,15 @@
 package Domain
 
 type UserViewModelInterface interface {
-	GetFirstName() string
-	GetLastName() string
-	GetBirthDate() string
-	GetPassport() PassportViewModelInterface
-	GetJobs() []JobsViewModelInterface
-	GetSex() string
-	GetMaritalStatus() string
-	GetChildren() int
+	UserModelInterface
 	Validate() bool
 	GetValidation() ValidationInterface
 }
 
 type PassportViewModelInterface interface {
-	GetSeries() int
-	GetNumber() int
+	PassportModelInterface
 }
 
 type JobsViewModelInterface interface {
-	GetName() string
-	GetWage() int
+	JobsModelInterface
 }
