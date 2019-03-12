46b2e7fd24291c394879e423f914acb67468df27
korwin <levozavr@mail.ru>
2019-03-12 12:19:32 +0300

    add user view model

--- Application/v1/viewModels/UserViewModel.go
+++ Application/v1/viewModels/UserViewModel.go
@@ -1,1 +1,59 @@
 package viewModels
+
+import "github.com/apmath-web/clients/Domain"
+
+type BasicUser struct {
+	firstName     string
+	lastName      string
+	birthDate     string
+	passport      Domain.PassportViewModelInterface
+	jobs          []Domain.JobsViewModelInterface
+	sex           string
+	maritalStatus string
+	children      int
+}
+
+type UserViewModel struct {
+	BasicUser
+	validation Domain.ValidationInterface
+}
+
+func (u *UserViewModel) GetFirstName() string {
+	return u.firstName
+}
+
+func (u *UserViewModel) GetLastName() string {
+	return u.lastName
+}
+
+func (u *UserViewModel) GetBirthDate() string {
+	return u.birthDate
+}
+
+func (u *UserViewModel) GetPassport() Domain.PassportViewModelInterface {
+	return u.passport
+}
+
+func (u *UserViewModel) GetJobs() []Domain.JobsViewModelInterface {
+	return u.jobs
+}
+
+func (u *UserViewModel) GetSex() string {
+	return u.sex
+}
+
+func (u *UserViewModel) GetMaritalStatus() string {
+	return u.maritalStatus
+}
+
+func (u *UserViewModel) GetChildren() int {
+	return u.children
+}
+
+func (u *UserViewModel) Validate() bool {
+	return true
+}
+
+func (u *UserViewModel) GetValidation() Domain.ValidationInterface {
+	return u.validation
+}
