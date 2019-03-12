2fd3af21fd0c97710a97b48d928b13528d576d99
korwin <levozavr@mail.ru>
2019-03-12 11:24:11 +0300

    add application model interfaces

--- Domain/applicationModelInterface.go
+++ Domain/applicationModelInterface.go
@@ -1,4 +1,35 @@
 package Domain
 
-type HelloWorldApplicationModel interface {
+type UserApplicationModel interface {
+	GetId() int
+	GetFirstName() string
+	GetLastName() string
+	GetBirthDate() string
+	GetPassport() PassportApplicationModel
+	GetJobs() []JobsApplicationModel
+	GetSex() string
+	GetMaritalStatus() string
+	GetChildren() int
+	SetFirstName(firstName string)
+	SetLastName(lastName string)
+	SetBirthDate(birthDate string)
+	SetPassport(passport PassportApplicationModel)
+	SetJobs(jobs []JobsApplicationModel)
+	SetSex(sex string)
+	SetMaritalStatus(status string)
+	SetChildren(number int)
+}
+
+type PassportApplicationModel interface {
+	GetSeries() int
+	GetNumber() int
+	SetNumber(number int)
+	SetSeries(series int)
+}
+
+type JobsApplicationModel interface {
+	GetName() string
+	GetWage() int
+	SetName(name string)
+	SetWage(wage int)
 }
