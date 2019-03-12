3af683912e3c333b03c7ef6bbcca1696da6b15ad
korwin <levozavr@mail.ru>
2019-03-12 13:56:08 +0300

    change interfaces fo json marshaling

--- Domain/validationInterface.go
+++ Domain/validationInterface.go
@@ -3,8 +3,10 @@ package Domain
 type ValidationInterface interface {
 	AddMessage(msg MessageInterface)
 	GetMessages() []MessageInterface
+	SetMessage(msg string)
+	MarshalJSON() (b []byte, e error)
 }
 
 type MessageInterface interface {
-	GetText() string
+	MarshalJSON() (b []byte, e error)
 }
