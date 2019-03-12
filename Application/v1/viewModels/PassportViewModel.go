8cea7f2904b19fbc0dfa4275d406ff3eb456096c
korwin <levozavr@mail.ru>
2019-03-12 12:11:35 +0300

    add passport view model

--- Application/v1/viewModels/PassportViewModel.go
+++ Application/v1/viewModels/PassportViewModel.go
@@ -1,1 +1,14 @@
 package viewModels
+
+type PassportViewModel struct {
+	series int
+	number int
+}
+
+func (p *PassportViewModel) GetSeries() int {
+	return p.series
+}
+
+func (p *PassportViewModel) GetNumber() int {
+	return p.number
+}
