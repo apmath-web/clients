859301fd4f8e6f58519a86bae6145d7175e43f08
korwin <levozavr@mail.ru>
2019-03-15 00:22:07 +0300

    add id error handling

--- Application/v1/actions/actionUpdate.go
+++ Application/v1/actions/actionUpdate.go
@@ -2,13 +2,24 @@ package actions
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/apmath-web/clients/Application/v1/Validation"
 	"github.com/apmath-web/clients/Application/v1/viewModels"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 func Update(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		validator := Validation.GenValidation()
+		validator.SetMessage("Validation error")
+		validator.AddMessage(Validation.GenMessage("id", err.Error()))
+		str, _ := json.Marshal(validator)
+		c.String(http.StatusBadRequest, string(str))
+		return
+	}
 	vm := new(viewModels.ClientViewModel)
 	if err := c.BindJSON(vm); err != nil {
 		validator := Validation.GenValidation()
@@ -25,6 +36,7 @@ func Update(c *gin.Context) {
 		c.String(http.StatusBadRequest, string(str))
 		return
 	}
+	fmt.Println(id)
 	c.String(http.StatusNoContent, "")
 
 }
