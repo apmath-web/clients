c89fbd6486b5bdfe5f30cec14c05f61a8bf794fb
korwin <levozavr@mail.ru>
2019-03-14 14:28:22 +0300

    change interfaces for view model

--- Domain/viewModelInterface.go
+++ Domain/viewModelInterface.go
@@ -1,21 +1,25 @@
 package Domain
 
-type ClientViewModelInterface interface {
-	ClientModelInterface
-	GetPassport() PassportViewModelInterface
-	GetJobs() []JobViewModelInterface
+type BasicViewModel interface {
 	Validate() bool
 	GetValidation() ValidationInterface
 	MarshalJSON() (b []byte, e error)
+}
+
+type ClientViewModelInterface interface {
+	BasicViewModel
+	ClientModelInterface
+	GetPassport() PassportViewModelInterface
+	GetJobs() []JobViewModelInterface
 	UnmarshalJSON(b []byte) error
 }
 
 type PassportViewModelInterface interface {
+	BasicViewModel
 	PassportModelInterface
-	MarshalJSON() (b []byte, e error)
 }
 
 type JobViewModelInterface interface {
 	JobModelInterface
-	MarshalJSON() (b []byte, e error)
+	BasicViewModel
 }
