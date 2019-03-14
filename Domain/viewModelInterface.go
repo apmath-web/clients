1b4592e9ee3637410421576b7468005cc3247e38
korwin <levozavr@mail.ru>
2019-03-14 13:23:16 +0300

    Change interface

--- Domain/viewModelInterface.go
+++ Domain/viewModelInterface.go
@@ -7,14 +7,17 @@ type ClientViewModelInterface interface {
 	Validate() bool
 	GetValidation() ValidationInterface
 	MarshalJSON() (b []byte, e error)
+	UnmarshalJSON(b []byte) error
 }
 
 type PassportViewModelInterface interface {
 	PassportModelInterface
 	MarshalJSON() (b []byte, e error)
+	UnmarshalJSON(b []byte) error
 }
 
 type JobViewModelInterface interface {
 	JobModelInterface
 	MarshalJSON() (b []byte, e error)
+	UnmarshalJSON(b []byte) error
 }
