e95ad4c13b1b0326e7b7732550d8215af5f3f8fd
korwin <levozavr@mail.ru>
2019-03-14 23:50:26 +0300

    add error handling in serialization

--- Application/v1/actions/actionCreate.go
+++ Application/v1/actions/actionCreate.go
@@ -1,10 +1,22 @@
 package actions
 
 import (
+	"encoding/json"
+	"github.com/apmath-web/clients/Application/v1/Validation"
+	"github.com/apmath-web/clients/Application/v1/viewModels"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func Create(c *gin.Context) {
-	c.String(http.StatusOK, "Hello world")
+	vm := new(viewModels.ClientViewModel)
+	if err := c.BindJSON(vm); err != nil {
+		validator := Validation.GenValidation()
+		validator.SetMessage("Validation error")
+		validator.AddMessage(Validation.GenMessage("json", err.Error()))
+		str, _ := json.Marshal(validator)
+		c.String(http.StatusBadRequest, string(str))
+		return
+	}
+
 }
