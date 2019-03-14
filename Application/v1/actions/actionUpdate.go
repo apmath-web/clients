a3d64fc829b8228cb6f96f55a6c939fb67016459
korwin <levozavr@mail.ru>
2019-03-15 00:18:52 +0300

    add error handling

--- Application/v1/actions/actionUpdate.go
+++ Application/v1/actions/actionUpdate.go
@@ -1,9 +1,22 @@
 package actions
 
 import (
+	"encoding/json"
+	"github.com/apmath-web/clients/Application/v1/Validation"
+	"github.com/apmath-web/clients/Application/v1/viewModels"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func Update(c *gin.Context) {
+	vm := new(viewModels.ClientViewModel)
+	if err := c.BindJSON(vm); err != nil {
+		validator := Validation.GenValidation()
+		validator.SetMessage("Validation error")
+		validator.AddMessage(Validation.GenMessage("json", err.Error()))
+		str, _ := json.Marshal(validator)
+		c.String(http.StatusBadRequest, string(str))
+		return
+	}
 
 }
