63a9f5226595d70d9e41a559119207ac4c28c349
korwin <levozavr@mail.ru>
2019-03-15 12:28:47 +0300

    add mock response

--- Application/v1/actions/actionGet.go
+++ Application/v1/actions/actionGet.go
@@ -2,7 +2,9 @@ package actions
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/apmath-web/clients/Application/v1/Validation"
+	"github.com/apmath-web/clients/Application/v1/viewModels"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"strconv"
@@ -18,5 +20,9 @@ func Get(c *gin.Context) {
 		c.String(http.StatusBadRequest, string(str))
 		return
 	}
-
+	fmt.Println(id)
+	vm := new(viewModels.ClientViewModel)
+	mockString := []byte(`{"firstName":"Lev","lastName":"Kovalenko","birthDate":"2019-01-02","Passport":{"series":2342,"number":434345},"Jobs":[{"name":"test1","wage":536446},{"name":"test4","wage":53644346}],"sex":"male","maritalStatus":"married","children":4}`)
+	_ = json.Unmarshal(mockString, vm)
+	c.JSON(http.StatusOK, vm)
 }
