fef2b245d9f5b310854515455e80fd78c22f8a52
korwin <levozavr@mail.ru>
2019-03-15 12:26:54 +0300

    delete hello world handler

--- Application/v1/routing/router.go
+++ Application/v1/routing/router.go
@@ -9,7 +9,6 @@ func GenRouter() *gin.Engine {
 	router := gin.Default()
 	v1 := router.Group("/v1")
 	{
-		v1.GET("/hello_world", actions.HelloWorldHandler)
 		v1.POST("/", actions.Create)
 		v1.PUT("/:id", actions.Update)
 	}
