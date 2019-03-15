77a65792a8fb2ca103089e15ff543a725e6ca117
korwin <levozavr@mail.ru>
2019-03-15 12:28:29 +0300

    add  get handler and id validation

--- Application/v1/actions/actionGet.go
+++ Application/v1/actions/actionGet.go
@@ -1,1 +1,22 @@
 package actions
+
+import (
+	"encoding/json"
+	"github.com/apmath-web/clients/Application/v1/Validation"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+func Get(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		validator := Validation.GenValidation()
+		validator.SetMessage("Validation error")
+		validator.AddMessage(Validation.GenMessage("id", err.Error()))
+		str, _ := json.Marshal(validator)
+		c.String(http.StatusBadRequest, string(str))
+		return
+	}
+
+}
