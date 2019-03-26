ff544965dec2367360a33c0deb8f2a756041d657
korwin <levozavr@mail.ru>
2019-03-26 12:24:59 +0300

    add bl for update

--- Application/v1/actions/actionUpdate.go
+++ Application/v1/actions/actionUpdate.go
@@ -3,8 +3,11 @@ package actions
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/apmath-web/clients/Application/v1/mapper"
 	"github.com/apmath-web/clients/Application/v1/validation"
 	"github.com/apmath-web/clients/Application/v1/viewModels"
+	"github.com/apmath-web/clients/Domain/models"
+	"github.com/apmath-web/clients/Infrastructure"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"strconv"
@@ -20,8 +23,8 @@ func Update(c *gin.Context) {
 		c.String(http.StatusBadRequest, string(str))
 		return
 	}
-	vm := new(viewModels.ClientViewModel)
-	if err := c.BindJSON(vm); err != nil {
+	vm := viewModels.ClientViewModel{}
+	if err := c.BindJSON(&vm); err != nil {
 		validator := validation.GenValidation()
 		validator.SetMessage("validation error")
 		validator.AddMessage(validation.GenMessage("json", err.Error()))
@@ -32,6 +35,17 @@ func Update(c *gin.Context) {
 	if !vm.Validate() {
 		validator := vm.GetValidation()
 		validator.SetMessage("validation error")
+		str, _ := json.Marshal(validator)
+		c.String(http.StatusBadRequest, string(str))
+		return
+	}
+	dm := mapper.ClientViewMapper(vm)
+	IdModel := models.GenId(id)
+	service := Infrastructure.GetServiceManager().GetClientService()
+	err = service.Update(IdModel, dm)
+	if err != nil {
+		validator := validation.GenValidation()
+		validator.SetMessage(err.Error())
 		str, _ := json.Marshal(validator)
 		c.String(http.StatusBadRequest, string(str))
 		return
