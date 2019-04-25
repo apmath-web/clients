70cf45163b97e4aefd2824fe25d49de4ec103cc8
korwin <levozavr@mail.ru>
2019-04-25 21:53:58 +0300

    add prototype of db working

--- Infrastructure/repositories/Repository.go
+++ Infrastructure/repositories/Repository.go
@@ -1,45 +1,86 @@
 package repositories
 
 import (
+	"database/sql"
 	"errors"
+	"fmt"
 	"github.com/apmath-web/clients/Domain"
+	"github.com/apmath-web/clients/Infrastructure/applicationModels"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"log"
 	"sync"
 )
 
 type clientRepository struct {
-	clients         map[int]Domain.ClientDomainModelInterface
-	NumberOfClients int
+	db *sqlx.DB
 }
 
 func (r *clientRepository) GetClient(id int) (Domain.ClientDomainModelInterface, error) {
-	client, ok := r.clients[id]
-	if ok {
-		return client, nil
-	}
 	return nil, errors.New("No client with such id")
 }
+
+func (r *clientRepository) insertClient(cl applicationModels.ClientApplicationModel) (int, error) {
+	tx, err := r.db.Begin()
+	if err != nil {
+		return 0, err
+	}
+	var clientId int
+	err = tx.QueryRow("INSERT INTO clients "+
+		"(first_name, last_name, birth_date, sex, marital_status, children) "+
+		"VALUES "+
+		"($1, $2, $3, $4, $5, $6) RETURNING id",
+		cl.GetFirstName(), cl.GetLastName(), cl.GetBirthDate(),
+		cl.GetSex(), cl.GetMaritalStatus(), cl.GetChildren()).Scan(&clientId)
+	fmt.Printf("%+v", clientId)
+	if err != nil {
+		return 0, err
+	}
+	if _, err = r.InsertPassport(cl.GetPassport(), tx, clientId); err != nil {
+		return 0, err
+	}
+	err = tx.Commit()
+	if err != nil {
+		return 0, err
+	}
+	return clientId, nil
+}
+
+func (r *clientRepository) InsertPassport(pas applicationModels.PassportApplicationModel, tx *sql.Tx, clientId int) (sql.Result, error) {
+	return tx.Exec("INSERT INTO passports "+
+		"(series, number, client_id) VALUES "+
+		"($1, $2, $3) RETURNING id",
+		pas.GetSeries(), pas.GetNumber(), clientId)
+}
+
 func (r *clientRepository) SetClient(model Domain.ClientDomainModelInterface) (int, error) {
-	r.NumberOfClients++
-	id := r.NumberOfClients
-	r.clients[id] = model
-	return id, nil
+	var client applicationModels.ClientApplicationModel
+	client.Hydrate(model)
+	fmt.Printf("%+v\n", model.GetPassport())
+	if id, err := r.insertClient(client); err != nil {
+		return 0, err
+	} else {
+		return id, nil
+	}
 }
 
 func (r *clientRepository) ChangeClient(id int, model Domain.ClientDomainModelInterface) error {
-	_, ok := r.clients[id]
-	if ok {
-		r.clients[id] = model
-		return nil
-	}
 	return errors.New("No client with such id")
 }
 
 var repo *clientRepository
 var once sync.Once
 
+const DB_CONNECT_STRING = "host=localhost port=5432 user=test password=test dbname=test sslmode=disable"
+
 func GenClientRepository() Domain.ClientRepositoryInterface {
 	once.Do(func() {
-		repo = &clientRepository{make(map[int]Domain.ClientDomainModelInterface), 0}
+		db, err := sqlx.Connect("postgres", DB_CONNECT_STRING)
+		fmt.Printf("%T", db)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		repo = &clientRepository{db}
 	})
 	return repo
 }
--- application.go
+++ application.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/apmath-web/clients/Application/v1/routing"
+	"github.com/apmath-web/clients/Infrastructure/repositories"
 	"log"
 )
 
 func main() {
+	repositories.GenClientRepository()
 	router := routing.GenRouter()
 	err := router.Run("0.0.0.0:8080")
 	if err != nil {
